01c4921cf9548d54e4030eefa0e59326146069e9
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2024-05-06 01:25:21 +0700

    feat(migration): ➕ add instances migration

---
+++ database/migrations/2024_05_05_181449_create_documents_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('instance_id')->constrained()->cascadeOnDelete();
+            $table->enum('type', ['incoming', 'outcoming']);
+            $table->string('number')->unique();
+            $table->date('date');
+            $table->text('subject');
+            $table->string('from');
+            $table->string('to');
+            $table->string('file');
+            $table->string('phone');
+            $table->mediumText('next_action')->nullable();
+            $table->mediumText('corrective_action')->nullable();
+            $table->mediumText('descripion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documents');
+    }
+};
